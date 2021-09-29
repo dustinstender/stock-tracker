e696cc0d34fb8fb432f4446276a8b047437ff23b
Dustin Stender <dustinpstender@gmail.com>
2021-09-29 19:14:38 +0200

    not working. need to fix the respond to stuff

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,11 @@
 class StocksController < ApplicationController
-
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
         flash[:alert] = "That ain't a stock symbol"
         redirect_to my_portfolio_path
