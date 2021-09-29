314dfcea5f329bd9e1d5e18fe28ad6a8d5aa21d7
Dustin Stender <dustinpstender@gmail.com>
2021-09-29 16:00:55 +0200

    setting up AJAX for the result request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,17 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "That ain't a stock symbol"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "You gotta be looking for something right?"
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,10 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
     secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
     endpoint: 'https://sandbox.iexapis.com/v1')
-
-    new(ticket: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticket: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
