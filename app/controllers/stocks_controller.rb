c3d5481d10f49a5aa10cc51b68227d6e1a4f31f0
Dustin Stender <dustinpstender@gmail.com>
2021-09-30 11:42:24 +0200

    no ajax :( but it works

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,9 +3,7 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
+        render 'users/my_portfolio'
       else
         flash[:alert] = "That ain't a stock symbol"
         redirect_to my_portfolio_path
