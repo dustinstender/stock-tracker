2a7f84decc63c9932e953d81ccbcf6beae035c55
Dustin Stender <dustinpstender@gmail.com>
2021-10-01 14:08:46 +0200

    added new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Stock.create([{ticket: 'AAPL', name: 'Apple Inc.'}, {ticket: 'TSLA', name: 'Tesla'}, {ticket: 'GOOG', name: 'Alphabet'}])
