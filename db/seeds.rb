aee09e166b7a89189b10771e0d5bc9749b13125f
Dustin Stender <dustinpstender@gmail.com>
2021-10-01 15:03:23 +0200

    my port view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Stock.create([{ticket: 'AAPL', name: 'Apple Inc.'}, {ticket: 'TSLA', name: 'Tesla'}, {ticket: 'GOOG', name: 'Alphabet'}])
+User.create(email: 'dustin@yahoo.com', password: '123456')
+Stock.create([{ ticket: 'AAPL', name: 'Apple Inc.'}, {ticket: 'TSLA', name: 'Tesla'}, {ticket: 'GOOG', name: 'Alphabet'}])
